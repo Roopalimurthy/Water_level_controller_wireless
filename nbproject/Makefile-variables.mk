0b969324a737f50b644b09788fe81f719bff1cd6
Roopalimurthy <roopalimurthy@gmail.com>
2025-08-14 19:01:36 +0530

    Add Rx

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=sap_working_wate_RX.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/sap_working_wate_RX.X.production.hex
